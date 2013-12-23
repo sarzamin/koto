e80144fd70d7c55fd5f8e109663f3228529cfb07
Dariush <dariushem@yahoo.com>
2013-12-23 22:47:46 +0330

    Add User Model with some blank function

---
+++ application/models/User.php
@@ -0,0 +1,23 @@
+<?php
+
+	class User extends CI_Model {
+		
+		function __construct(){
+			// Call CI_Model constructor
+        	parent::__construct();
+
+		}//end constructor
+
+		function addUser(){
+
+		}//end function addUser
+
+		function editUser(){
+
+		}//end function editUser
+
+		function userInfo(){
+
+		}//end function userInfo
+
+	}//end class user

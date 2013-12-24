d0988f02debf8584d675152418a4c34808bf5f25
Dariush <dariushem@yahoo.com>
2013-12-24 11:33:16 +0330

    Edit 'addUser' function in User model complete 'editUser' function in User model

--- application/models/User.php
+++ application/models/User.php
@@ -8,7 +8,9 @@
 
 		}//end constructor
 
-		function addUser($username,$password,$email){
+
+		function addUser($username,$password,$email)
+		{
 			
 			//User's basic information
 			$this->username = $username;
@@ -18,21 +20,52 @@
 
 			//array of user's data
 			$userDataArray = array(
-				'username' = $this->username,
-				'password' = $this->password,
-				'email' = $this->email,
-				'signupDate' = $this->currentTime
+				'username' => $this->username,
+				'password' => $this->password,
+				'email' => $this->email,
+				'signupDate' => $this->currentTime
 				);
 
 			//Insert $userDataArray to users table
 			$this->db->insert('users', $userDataArray);
 		}//end function addUser
 
-		function editUser(){
+
+		function editUser($userID,$name,$email,$url,$location,$about)
+		{
+			//user's information for update users table
+			$this->name = $name;
+			$this->email = $email;
+			$this->url = $url;
+			$this->location = $location;
+			$this->about = $about;
+
+			//array of user's data
+			$userDataArray = array(
+				'name' => $this->name,
+				'email' => $this->email,
+				'url' => $this->url,
+				'location' => $this->location,
+				'about' => $this->about
+				 );
+
+			//select user row
+			$this->db->where('userID',$userID);
+
+			//update selected row
+			$this->db->update('users', $userDataArray); 
 
 		}//end function editUser
 
-		function userInfo(){
+
+		function changePassword()
+		{
+
+		}
+
+
+		function userInfo()
+		{
 
 		}//end function userInfo
 

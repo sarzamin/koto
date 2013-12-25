183310901bb2ca66f7482a5695fb977e0058f610
Dariush <dariushem@yahoo.com>
2013-12-25 13:28:20 +0330

    complete 'addPost' function in 'Post' model

--- application/models/Post.php
+++ application/models/Post.php
@@ -6,18 +6,42 @@
 
 		function __construct(){
 			parent::__construct();
-		}
+		}//end construct
 
-		function addPost(){
+		function addPost($postTitle,$postHead,$postBody,$authorID,$postCreateTime,$active){
 
-		}
+			//post information
+			$this->postTitle = $postTitle;
+			$this->postHead = $postHead;
+			$this->postBody = $postBody;
+			$this->authorID = $authorID;
+			$this->postCreateTime = $postCreateTime;
+			$this->active = $active;
+
+			//array of post info
+			$postDataArray =  array(
+				'postTitle' => $this->postTitle,
+				'postHead' => $this->postHead,
+				'postBody' => $this->postBody,
+				'authorID' => $this->authorID,
+				'postCreateTime' => $this->postCreateTime,
+				'active' => $this->active );
+
+			//Insert post to database
+			$this->db->insert('posts',$postDataArray);
+
+		}//end addPost function
 
 		function editPost(){
 
-		}
+		}//end editPost function
 
 		function deletePost(){
 
-		}
+		}//end deletePost function
+
+		function showPosts(){
+
+		}//end showPosts function
 
 	}//end class

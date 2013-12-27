dfde7fad7ef14a269a9aee63bba48f632da95f8d
Dariush <dariushem@yahoo.com>
2013-12-27 21:53:21 +0330

    add 'tag' model with some blank function

---
+++ application/models/Tag.php
@@ -0,0 +1,25 @@
+<?php
+
+	class Tag extends CI_Model{
+
+		function __construct(){
+			parent::__construct();
+		}//end construct
+
+		function addTag(){
+
+		}//end function
+
+		function removeTag(){
+
+		}//end function
+
+		function showTags(){
+
+		}//end function
+
+		function showTagItems(){
+
+		}//end function
+
+	}//end class

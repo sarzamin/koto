7b16d37bdb62f61ad68772e9ffda017a0bad0293
Dariush <dariushem@yahoo.com>
2013-12-29 22:16:51 +0330

    Create a blank migration for users

---
+++ application/migrations/001_users.php
@@ -0,0 +1,18 @@
+<?php 
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+
+class Users_Table extends CI_Migration {
+
+	public function up()
+	{
+
+	}//end function
+
+	public function down()
+	{
+
+	}//end function
+
+}//end class

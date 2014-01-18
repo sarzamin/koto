f4ae766242bcba42343824ef3d00ab573d65c8f5
Dariush <dariushem@yahoo.com>
2014-01-18 23:24:55 +0330

    Add Backend_Controller and Frontend_Controller to library

---
+++ application/libraries/Backend_Controller.php
@@ -0,0 +1,9 @@
+<?php
+
+class Backend_Controller extends MY_Controller {
+
+	function __construct(){
+		parent::__construct();
+	}
+
+}
---
+++ application/libraries/Frontend_Controller.php
@@ -0,0 +1,10 @@
+<?php
+
+class Frontend_Controller extends MY_Controller {
+
+	function __construct(){
+		parent::__construct();
+	}
+
+
+}

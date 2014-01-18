d203fe122ee35e19589f715343486568bf7ad1c7
Dariush <dariushem@yahoo.com>
2014-01-18 23:26:23 +0330

    edit MY_Controller =)

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -4,8 +4,8 @@ class MY_Controller extends CI_Controller {
 
 	public $data = array();
 
-	function __constructor(){
-		parent::__constructor();
+	function __construct(){
+		parent::__construct();
 		$this->data['errors'] = array();
 		$this->data['site_title'] = config_item('koto_title');
 	}

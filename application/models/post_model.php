2102737dbcc6804e4bc37fa2f508f3c8523745a4
Dariush <dariushem@yahoo.com>
2014-02-24 11:19:48 +0330

    restart\!

--- application/models/post_model.php
+++ application/models/post_model.php
@@ -1,61 +1,33 @@
 <?php
+	/**
+	 * @author edrock
+	 */
+	class post_model extends CI_Model {
 
-	/*
-	*/
-	class post_model extends MY_Model {
-
-		protected $postID;
-		protected $postTitle;
-		protected $postHead;
-		protected $postBody;
-		protected $authorID;
-		protected $postCreateTime;
-		protected $postModifyTime;
-		protected $active;
-
-		function __construct(){
+		$tablename = '';
+		
+		function __construct() {
 			parent::__construct();
-		}//end construct
+		}
+		
+		function add($data)
+		{
+			$this->db->insert('posts',$data);
+		}
+		
+		function remove($postID)
+		{
+			$where = array('id' => $postID);
+				
+			$this->db->where($where);
+			$this->db->delete('posts'); 
+		}
 
-		function addPost($postTitle,$postHead,$postBody,$authorID,$postCreateTime,$active){
+		function update($postID,$newdata){
+			$where = array('id' => $postID );
 
-			//post information array
-			$postData = array(
-				'postTitle' => $postTitle,
-				'postHead' => $postHead,
-				'postBody' => $postBody,
-				'authorID' => $authorID,
-				'postCreateTime' => $postCreateTime,
-				'active' => $active);
-
-			//Insert post to database with object
-			return $this->insert($postData);
-
-		}//end addPost function
-
-		function editPost($postID,$postTitle,$postHead,$postBody,$authorID,$postCreateTime,$postModifyTime,$active){
-
-			// post infomation array
-			$postData = array(
-				'postTitle' => $postTitle,
-				'postHead' => $postHead,
-				'postBody' => $postBody,
-				'authorID' => $authorID,
-				'postCreateTime' => $postCreateTime,
-				'postModifyTime' => $postModifyTime,
-				'active' => $active);
-
-			//update post info with object
-			return $this->update($postID,$postData);
-		}//end editPost function
-
-		function deletePost($postID){
-			return $this->delete($postID);
-		}//end deletePost function
-
-		function showPosts($numberOfPosts,$start){
-            $result = $this->db->get($_table)->where('id<=' = $start)->limit($numberOfPosts);
-            return $result;
-		}//end showPosts function
-
-	}//end class
+			$this->db->where($where);
+			$this->db->update('posts', $newdata); 
+		}
+	}
+	
--- application/models/user_model.php
+++ application/models/user_model.php
@@ -1,86 +1,33 @@
 <?php
-
-	/*
-	*/
+	/**
+	 * @author edrock
+	 */
 	class user_model extends CI_Model {
 		
-		function __construct(){
-			// Call CI_Model constructor
-        	parent::__construct();
-
-		}//end constructor
-
-
-		function addUser($username,$password,$email)
+		function __construct() {
+			parent::__construct();
+		}
+		
+		function register($data)
 		{
-			
-			//User's basic information
-			$this->username = $username;
-			$this->password = sha1($password);
-			$this->email = $email;
-			$this->currentTime = time();
-
-			//array of user's data
-			$userDataArray = array(
-				'username' => $this->username,
-				'password' => $this->password,
-				'email' => $this->email,
-				'signupDate' => $this->currentTime
-				);
-
-			//Insert $userDataArray to users table
-			$this->db->insert('users', $userDataArray);
-		}//end function addUser
-
-
-		function editUser($userID,$name,$email,$url,$location,$about)
+			$this->db->insert('users',$data);
+		}
+		
+		function login($username,$password)
 		{
-			//user's information for update users table
-			$this->name = $name;
-			$this->email = $email;
-			$this->url = $url;
-			$this->location = $location;
-			$this->about = $about;
+			$where = array('username' => $username,
+				'password' => sha1($password) );
+				
+			$this->db->select()->from('users')->where($where);
+			$query = $this->db->get();
+			return $query->first_row('array');
+		}
 
-			//array of user's data
-			$userDataArray = array(
-				'name' => $this->name,
-				'email' => $this->email,
-				'url' => $this->url,
-				'location' => $this->location,
-				'about' => $this->about
-				 );
+		function editInfo($userID,$newdata){
+			$where = array('id' => $userID );
 
-			//select user row
-			$this->db->where('userID',$userID);
-
-			//update selected row
-			$this->db->update('users', $userDataArray); 
-
-		}//end function editUser
-
-
-		function changePassword($userID,$currentPassword,$newPassword)
-		{
-			//check current password accuracy
-			$selectRow = $this->db->get_where('users',array('userID',$userID));
-
-			if(sha1($currentPassword)===sha1($selectRow->row(0))){
-
-				//update password
-				$this->db->where('userID',$userID);
-				$this->db->update('users',sha1($newPassword));
-
-			}//end if
-		}//end function changePassword
-
-
-		function userInfo($userID)
-		{
-			//select user
-			$selectUser=$this->db->get_where('users',array('userID',$userID));
-			return $selectUser->result_array();
-
-		}//end function userInfo
-
-	}//end class user
+			$this->db->where($where);
+			$this->db->update('users', $newdata); 
+		}
+	}
+	

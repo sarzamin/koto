8cf48ec7638cc0701337d30c0d7b34573dee56b1
Dariush <dariushem@yahoo.com>
2014-02-28 16:57:40 +0330

    merge

--- application/models/post_model.php
+++ application/models/post_model.php
@@ -33,6 +33,9 @@
 		}
 
 		function update($postID,$newdata){
+			$this->db->where($where);
+			$this->db->delete('postTags');
+			
 			$where = array('id' => $postID );
 
 			$this->db->where($where);

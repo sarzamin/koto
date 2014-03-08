e10beaf1d89a93e4dbf9b3bb3ce84aa7a52583cf
sajad <sajad@shokri.net>
2014-03-09 00:09:56 +0330

    dariush knows!!

--- application/controllers/posts.php
+++ application/controllers/posts.php
@@ -7,7 +7,19 @@ class Posts extends CI_Controller {
         $data['posts'] = $this->Post->showPosts($numberOfPosts,$startID); 
 
     }//end of index function
-
+    
+    function addpost(){
+        $data['add'] = 0;
+        if($this->session->userdata('userID')&& !$_POST)
+        {
+            $this->load->view('',$data);//set view of form post
+        }elseif($this->session->userdata('userID')&& $_POST){
+            
+        }else{
+            
+        }
+        
+    }
 
     function findTags($content){
 
